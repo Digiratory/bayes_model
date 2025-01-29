5e28a3d8692f52d62545dbf8cef97085cefe958c
irinaspakovskaa <3533868@gmail.com>
2025-01-29 23:20:52 +0300

    fix subplots optimisation

--- bn_modeller/widgets/plots/pairplot_view.py
+++ bn_modeller/widgets/plots/pairplot_view.py
@@ -7,146 +7,115 @@ from PySide6.QtWidgets import QFrame, QWidget, QVBoxLayout
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
-import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib
 import numpy as np
 matplotlib.use('Qt5Agg')
 
+
 class PairplotMplCanvas(FigureCanvasQTAgg):
+    def __init__(self, parent=None, width=12, height=12, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        super().__init__(self.fig)
+        self.axes = None
 
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super().__init__(fig)
-
-
-class PairplotView(QFrame):
-    def __init__(self, parent: QWidget = None, f=Qt.WindowType()):
-        super().__init__(parent, f)
-        self._model: RelationalSortFilterProxyModel = None
-        self.mplCanvas = None
-        self.init_ui()
-
-    def init_ui(self):
-        self.mainLayout = QVBoxLayout()
-
-        # self.mplCanvas = PairplotMplCanvas(self)
-
-        self.setLayout(self.mainLayout)
-
-    def setModel(self, model: RelationalSortFilterProxyModel, hue_names_col: int, sample_id_col: int, hue_id_col: int, value_col: int):
-        if self._model is not None:
-            # TODO: replace with builtin Qt Signal
-            self._model.filterInvalidated.disconnect(self.updateVisualization)
-        self._model = model
-        self._model.filterInvalidated.connect(self.updateVisualization)
-        
-        self._value_col = value_col
-        self._sample_id_col = sample_id_col
-        self._hue_id_col = hue_id_col
-        self._hue_names_col = hue_names_col
-
-
-    # @Slot("QList<QPersistentModelIndex>", QAbstractItemModel.LayoutChangeHint)
-    # def updateVisualization(self, parents: list[QPersistentModelIndex] = [],
-    #                         hint: QAbstractItemModel.LayoutChangeHint = QAbstractItemModel.LayoutChangeHint.NoLayoutChangeHint):
-
-    # @Slot(QModelIndex, QModelIndex, "QList<int>")
-    # def updateVisualization(self, topLeft: QModelIndex = None, bottomRight: QModelIndex = None, roles: list[int] = None):
-
-    @Slot()
-    def updateVisualization(self):
-        filterModel = self._model.filterModel()
-        filter_labels = {}
-        for rowIdx in range(filterModel.rowCount()):
-            k = filterModel.data(
-                filterModel.index(rowIdx, self._model.filterValueColumn()))
-            v = filterModel.data(filterModel.index(
-                rowIdx, self._hue_names_col))
-
-
-            filter_labels[k] = v
-
-        data_dict = {"sample": [],
-                     "value": [],
-                     "label": []}
-        for rowIdx in range(self._model.rowCount()):
-            data_dict["sample"].append(self._model.data(
-                self._model.index(rowIdx, self._sample_id_col)))
-            data_dict["label"].append(filter_labels[self._model.data(
-                self._model.index(rowIdx, self._hue_id_col))])
-            data_dict["value"].append(self._model.data(
-                self._model.index(rowIdx, self._value_col)))
-        data_pd = pd.DataFrame.from_dict(data_dict)
-        data_pd = data_pd.pivot(
-            index="sample", columns="label", values="value")
-
+    def update_plot(self, data_pd):
         if len(data_pd) == 0:
             return
 
-        # фильтрация признаков при построении графиков
-        filtered_order = [col for col in list(filter_labels.values()) if col in data_pd.columns]
-        data_pd = data_pd[filtered_order]
         labels = data_pd.columns
         num_vars = len(labels)
 
-        fig, axes = plt.subplots(num_vars, num_vars, figsize=(12, 12))
+        self.fig.clear()
 
+        # Create new subplots
+        if num_vars == 1:
+            self.axes = self.fig.add_subplot(111)
+        else:
+            self.axes = self.fig.subplots(num_vars, num_vars)
 
         for i in range(num_vars):
             for j in range(num_vars):
-                ax = axes[i, j] if isinstance(axes, np.ndarray) else axes
+                if num_vars == 1:
+                    ax = self.axes
+                else:
+                    ax = self.axes[i, j]
 
                 if i == j:
                     ax.hist(data_pd.iloc[:, i])
-                    # ax.hist(data_pd.iloc[:, i], bins=20, color='lightgray')
-                    # ax.set_title(labels[i])
-
-                    if j == 0:  # Подпись OY только на первом столбце
+                    if j == 0:
                         ax.set_ylabel(labels[i])
-                    if i == num_vars - 1:  # Подпись OX только на последней строке
+                    if i == num_vars - 1:
                         ax.set_xlabel(labels[j])
                 else:
                     ax.scatter(data_pd.iloc[:, j], data_pd.iloc[:, i], alpha=0.6)
-                    # ax.set_xlabel(labels[j])
-                    # ax.set_ylabel(labels[i])
+                    ax.set_xlabel('' if i != num_vars - 1 else labels[j])
+                    ax.set_ylabel('' if j != 0 else labels[i])
 
-                    ax.set_xlabel('' if i != num_vars - 1 else labels[j])  # Подпись OX только для нижней строки
-                    ax.set_ylabel('' if j != 0 else labels[i])  # Подпись OY только для левого столбца
-
-                    # Добавление корреляции
-                    df1 = data_pd.iloc[:, i]
-                    df2 = data_pd.iloc[:, j]
-
+                    # Add correlation
+                    df1, df2 = data_pd.iloc[:, i], data_pd.iloc[:, j]
+                    # df1_cleaned, df2_cleaned = df1.align(df2, join='inner')
                     df1_cleaned, df2_cleaned = df1.dropna(), df2.dropna()
 
                     # Убираем те строки, в которых один из столбцов имеет NaN
                     df1_cleaned, df2_cleaned = df1_cleaned.align(df2_cleaned, join='inner')
 
                     corr = np.corrcoef(df1_cleaned, df2_cleaned)[0, 1]
-                    ax.annotate(f'Corr: {corr:.2f}', xy=(0.5, 0.9), xycoords='axes fraction', ha='center', fontsize=10,
-                                color='red')
+                    ax.annotate(f'Corr: {corr:.2f}', xy=(0.5, 0.9), xycoords='axes fraction', ha='center', fontsize=10, color='red')
 
-        plt.tight_layout(pad=3.0)
-        fig.subplots_adjust(hspace=0.3, wspace=0.3)
+        self.fig.tight_layout(pad=3.0)
+        self.fig.subplots_adjust(hspace=0.3, wspace=0.3)
+        self.draw()
 
-        newMplCanvas = FigureCanvasQTAgg(fig)
 
-        # панель инструментов
-        toolbar = NavigationToolbar2QT(newMplCanvas, self)
+class PairplotView(QFrame):
+    def __init__(self, parent: QWidget = None, f=Qt.WindowType()):
+        super().__init__(parent, f)
+        self._model: RelationalSortFilterProxyModel = None
+        self.init_ui()
 
-        if hasattr(self, 'toolbar'):
-            self.mainLayout.replaceWidget(self.toolbar, toolbar)
-        else:
-            self.toolbar = toolbar
-            self.mainLayout.addWidget(self.toolbar)
-
-        if self.mplCanvas is not None:
-            self.mainLayout.replaceWidget(self.mplCanvas, newMplCanvas)
-            del self.mplCanvas
-        self.mplCanvas = newMplCanvas
+    def init_ui(self):
+        self.mainLayout = QVBoxLayout()
+        self.mplCanvas = PairplotMplCanvas(self)
+        self.toolbar = NavigationToolbar2QT(self.mplCanvas, self)
+        self.mainLayout.addWidget(self.toolbar)
         self.mainLayout.addWidget(self.mplCanvas)
+        self.setLayout(self.mainLayout)
+
+    def setModel(self, model: RelationalSortFilterProxyModel, hue_names_col: int, sample_id_col: int, hue_id_col: int,
+                 value_col: int):
+        if self._model is not None:
+            self._model.filterInvalidated.disconnect(self.updateVisualization)
+        self._model = model
+        self._model.filterInvalidated.connect(self.updateVisualization)
+
+        self._value_col = value_col
+        self._sample_id_col = sample_id_col
+        self._hue_id_col = hue_id_col
+        self._hue_names_col = hue_names_col
+
+    @Slot()
+    def updateVisualization(self):
+        filterModel = self._model.filterModel()
+        filter_labels = {filterModel.data(filterModel.index(rowIdx, self._model.filterValueColumn())):
+                             filterModel.data(filterModel.index(rowIdx, self._hue_names_col))
+                         for rowIdx in range(filterModel.rowCount())}
+
+        data_dict = {"sample": [], "value": [], "label": []}
+        for rowIdx in range(self._model.rowCount()):
+            data_dict["sample"].append(self._model.data(self._model.index(rowIdx, self._sample_id_col)))
+            data_dict["label"].append(filter_labels[self._model.data(self._model.index(rowIdx, self._hue_id_col))])
+            data_dict["value"].append(self._model.data(self._model.index(rowIdx, self._value_col)))
+
+        data_pd = pd.DataFrame.from_dict(data_dict).pivot(index="sample", columns="label", values="value")
+
+        if len(data_pd) == 0:
+            return
+
+        filtered_order = [col for col in filter_labels.values() if col in data_pd.columns]
+        data_pd = data_pd[filtered_order]
+
+        self.mplCanvas.update_plot(data_pd)
 
 
 
