a119ed3268290576f84e2c22a551948a52501e36
Александр <siniza.s.94@gmail.com>
2025-02-14 21:42:08 +0300

    doc: add description for QSeparator class

--- bn_modeller/widgets/separator_widget.py
+++ bn_modeller/widgets/separator_widget.py
@@ -3,6 +3,17 @@ from PySide6.QtWidgets import QFrame, QWidget
 
 
 class QSeparator(QFrame):
+    """ A separator widget.
+    A separator is a line  that are used to separate
+    different parts of the user interface.
+
+    Usage example:
+    
+    >>> separator = QSeparator()
+    >>> layout.addWidget(separator)
+
+    """
+
     def __init__(self,
                  parent: QWidget | None = None,
                  f: Qt.WindowType = Qt.WindowType()):
